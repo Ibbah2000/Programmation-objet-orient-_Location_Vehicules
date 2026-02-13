2ade9a2798540e61ec066fc866487db9815bb1c1
Ibbah2000 <ibrahimacaba.bah@etu.univ-tours.fr>
2026-02-13 14:48:57 +0100

    Ibrahima 04-serialisation-client-vehicule

--- models/client.py
+++ models/client.py
@@ -9,3 +9,20 @@ class Client:
 
     def __str__(self):
         return f"{self.id_client}\n{self.prenom} {self.nom} ({self.mail})"
+
+    def to_dict(self):
+        return {
+            "id_client": self.id_client,
+            "nom": self.nom,
+            "prenom": self.prenom,
+            "mail": self.mail,
+            "telephone": self.telephone,
+            "adresse": self.adresse
+        }
+
+    @staticmethod
+    def from_dict(data):
+        return Client(
+            data['id_client'], data['nom'], data['prenom'],
+            data['mail'], data['telephone'], data['adresse']
+        )
